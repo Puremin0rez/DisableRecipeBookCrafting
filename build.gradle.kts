001e6e9a25c0eb869d51364566caa8164eb4e72b
Dylan Xaldin <Puremin0rez515@gmail.com>
2021-07-02 02:19:17 -0500

    Switch build tooling to Gradle Goodbye Maven, it was fun while it lasted.

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("java-library")
+}
+
+group = "com.dethemium"
+version = "1.0"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
+    }
+}
+
+repositories {
+    mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/repositories/snapshots/")
+    maven("https://repo.dmulloy2.net/repository/public/")
+}
+
+dependencies {
+    compileOnly(group = "org.spigotmc", name = "spigot-api", version = "1.12-R0.1-SNAPSHOT")
+    compileOnly(group = "com.comphenix.protocol", name = "ProtocolLib", version = "4.6.0")
+}
+
+defaultTasks("clean", "build")
+
+tasks {
+    processResources {
+        val placeholders = mapOf(
+            "name" to project.name,
+            "group" to project.group,
+            "version" to project.version
+        )
+        filesMatching("plugin.yml") {
+            expand(placeholders)
+        }
+    }
+
+    jar {
+        archiveFileName.set("${project.name}.jar")
+    }
+}
